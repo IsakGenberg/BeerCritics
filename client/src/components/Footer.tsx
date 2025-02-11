6c7c8b82608133b63a00c4440652d48022d6c8a2
erbergli <erik.berglind@gmail.com>
2025-02-11 15:34:58 +0100

    Add first draft of footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import { Container, Row, Col, Nav } from "react-bootstrap";
+// import './styles/Footer.css'
+
+function Footer() {
+  return (
+    <Container>
+      <Row fluid>
+        <Col>
+          <h1>BeerCritics</h1>
+          <p>We critique beer</p>
+        </Col>
+        <Col> 
+            <Nav className = "flex-column">
+                <h4>
+                    Useful links
+                </h4>
+                <Nav.Link>
+                    Home
+                </Nav.Link>
+                <Nav.Link>
+                    About us
+                </Nav.Link>
+                <Nav.Link>
+                    All Beers
+                </Nav.Link>
+            </Nav>
+        </Col>
+        <Col> <h4>Drunk call us!</h4>
+        <p>+11235423</p> </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Footer from './components/Footer.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Footer />
   </StrictMode>,
 )
