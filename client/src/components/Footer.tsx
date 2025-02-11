cfd456425cdd7d6e1f2118f0adba16238b2e3278
erbergli <erik.berglind@gmail.com>
2025-02-11 15:35:34 +0100

    format

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -9,24 +9,18 @@ function Footer() {
           <h1>BeerCritics</h1>
           <p>We critique beer</p>
         </Col>
-        <Col> 
-            <Nav className = "flex-column">
-                <h4>
-                    Useful links
-                </h4>
-                <Nav.Link>
-                    Home
-                </Nav.Link>
-                <Nav.Link>
-                    About us
-                </Nav.Link>
-                <Nav.Link>
-                    All Beers
-                </Nav.Link>
-            </Nav>
+        <Col>
+          <Nav className="flex-column">
+            <h4>Useful links</h4>
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link>About us</Nav.Link>
+            <Nav.Link>All Beers</Nav.Link>
+          </Nav>
         </Col>
-        <Col> <h4>Drunk call us!</h4>
-        <p>+11235423</p> </Col>
+        <Col>
+          <h4>Drunk call us!</h4>
+          <p>+11235423</p>
+        </Col>
       </Row>
     </Container>
   );
