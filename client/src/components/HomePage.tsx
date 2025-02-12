1f313553f712f513baa94d6bf2872025cfd3c646
erbergli <erik.berglind@gmail.com>
2025-02-12 11:45:13 +0100

    add homepage

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import SearchBar from "./SearchBar";
+
+
+function HomePage(){
+    return(
+        <Container>
+            <SearchBar/>
+        </Container>
+    );
+}
+
+export default HomePage;
