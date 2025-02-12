0af9ad503e9fdfdc36d3e0161e28e33b061d4ea4
erbergli <erik.berglind@gmail.com>
2025-02-12 10:50:48 +0100

    format and add comment

--- client/src/_tests/Footer.test.tsx
+++ client/src/_tests/Footer.test.tsx
@@ -1,9 +1,9 @@
 // TODO write test for links to see if they lead to the correct path?
 import Footer from "../components/Footer";
-import { render} from '@testing-library/react';
+import { render } from "@testing-library/react";
 
 describe("Footer", () => {
-    it("renders correctly", () => {
-        render(<Footer/>);
-    });
-})
+  it("renders correctly", () => {
+    render(<Footer />);
+  });
+});
