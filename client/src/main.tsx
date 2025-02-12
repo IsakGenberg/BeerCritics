d468f336c3de1055fc05fb829322da9155923236
hannistuis <hannah.tuu.hannah@gmail.com>
2025-02-12 10:14:16 +0100

    navbar
    frontend for navbar done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/Navbar";
+// import "./styles/navbar.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MyNavbar />
+  </StrictMode>
+);
