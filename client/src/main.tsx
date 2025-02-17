506fe5b0dc2b45ffe9611a6fdbd1007fa2340a18
erbergli <112847802+erbergli@users.noreply.github.com>
2025-02-17 14:20:23 +0100

    Add layout to main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,14 +8,17 @@ import Footer from './components/Footer.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BeerPage from './pages/BeerPage'
+import { Layout } from "./components/Layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
 <StrictMode>
   <BrowserRouter>
-    <MyNavbar />
-    <Routes>
-      <Route path="/beer/:name" element={<BeerPage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/beer/:name" element={<BeerPage />} />
+      </Routes>
+      <SearchBar/>
+    </Layout>
   </BrowserRouter>
 </StrictMode>
 )
