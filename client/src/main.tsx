1c99094d7a777d43432738584cfc49b659334b36
epicreach <luqas.lundahl4817@gmail.com>
2025-02-17 11:11:48 +0100

    Added searchbar functionality

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,18 +4,18 @@ import "./styles/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/Navbar";
 import SearchBar from "./components/SearchBar.tsx";
-import Footer from './components/Footer.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./components/Footer.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import BeerPage from './pages/BeerPage'
+import BeerPage from "./pages/BeerPage";
 
 createRoot(document.getElementById("root")!).render(
-<StrictMode>
-  <BrowserRouter>
-    <MyNavbar />
-    <Routes>
-      <Route path="/beer/:name" element={<BeerPage />} />
-    </Routes>
-  </BrowserRouter>
-</StrictMode>
-)
+  <StrictMode>
+    <BrowserRouter>
+      <MyNavbar />
+      <Routes>
+        <Route path="/beer/:name" element={<BeerPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
