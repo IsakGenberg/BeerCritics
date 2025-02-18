33982ad179a41339208c12c07c6043e2e23c2270
epicreach <luqas.lundahl4817@gmail.com>
2025-02-18 15:54:12 +0100

    added setup for lab4

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,28 @@
-import express from 'express';
-import { beerRouter } from './router/beer';
+import dotenv from "dotenv";
+import express from "express";
+import { beerRouter } from "./router/beer";
 import cors from "cors";
+import session from "express-session";
 
 export const app = express();
-
+dotenv.config();
 app.use(express.json());
-app.use(cors());
+
+if (!process.env.SESSION_SECRET) {
+  console.log("Could not find SESSION_SECRET in .env file");
+  process.exit();
+}
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use("/beer", beerRouter);
