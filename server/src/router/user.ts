8362f1a04d4aab6e7be4458f839a512e4e16a431
epicreach <luqas.lundahl4817@gmail.com>
2025-02-21 14:37:13 +0100

    updated review and user services, updated beer services

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -5,19 +5,28 @@ import { User } from "../model/user";
 export const userRouter = express.Router();
 const userService = new UserService();
 
-
-
+userRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const { user } = req.body;
+    await userService.registerUser(user);
+    res.status(201).send(`Registered user`);
+  } catch (e: any) {
+    res.status(500).send(e.message);
+  }
+});
 
 userRouter.post(
-    "/",
-    async (req: Request, res: Response) => {
-      try {
-        const { user } = req.body; 
-        await userService.registerUser(user);
-        res.status(201).send(`Registered user`);
-      } catch (e: any) {
-        res.status(500).send(e.message);
-      }
+  "/:name",
+  async (
+    req: Request<{}, {}, { username: string; password: string }>,
+    res: Response
+  ) => {
+    try {
+      const { username, password } = req.body;
+      const correctPwrd = await userService.checkPassword(username, password);
+      res.status(200).send(correctPwrd);
+    } catch (e: any) {
+      res.status(500).send(e.message);
     }
-  );
-  
+  }
+);
--- server/src/service/beerService.ts
+++ server/src/service/beerService.ts
@@ -1,65 +1,48 @@
 import { Beer } from "../model/beer";
 import path from "path";
 import dotenv from "dotenv";
+import { readFile, readFileSync } from "fs";
 
-const fs = require('fs');
+const fs = require("fs");
 dotenv.config();
 
 export class BeerService {
-  private beers: Beer[] = [
-    {
-      name: "Heineken",
-      rating: 4,
-      brewery: "Stockholm brewery",
-      style: "Lager",
-      abv: 5,
-      imagePath: "/heineken.png",
-    },
-    {
-      name: "Corona",
-      rating: 3,
-      brewery: "Stockholm brewery",
-      style: "Mexian Beer",
-      abv: 2,
-      imagePath: "/corona.png",
-    },
-    {
-      name: "Guinness",
-      rating: 2,
-      brewery: "Ireland brewery",
-      style: "Stout",
-      abv: 5,
-      imagePath: "/guinness.png",
-    },
-  ];
-
-  async addBeer(beer: Beer): Promise<void>{
+  async addBeer(beer: Beer): Promise<void> {
     const filePath = process.env.BEERS_JSON_PATH;
-  
-      if (!filePath) {
-          throw new Error("BEERS_JSON_PATH is not defined in the .env file");
-      }
-  
-      const absolutePath = path.resolve(filePath); 
-  
-      try {
-          let data = fs.readFileSync(absolutePath, "utf-8");
-          let users = JSON.parse(data);
-          users.push(beer);
-  
-          fs.writeFileSync(absolutePath, JSON.stringify(users, null, 2));
-          console.log("Beer added successfully!");
-      } catch (err) {
-          console.error("Error accessing users.json:", err);
-      }
-  
+
+    if (!filePath) {
+      throw new Error("BEERS_JSON_PATH is not defined in the .env file");
+    }
+    const absolutePath = path.resolve(filePath);
+
+    try {
+      let data = fs.readFileSync(absolutePath, "utf-8");
+      let beers = JSON.parse(data);
+      beers.push(beer);
+
+      fs.writeFileSync(absolutePath, JSON.stringify(beers, null, 2));
+      console.log("Beer added successfully!");
+    } catch (err) {
+      console.error("Error accessing users.json:", err);
+    }
   }
 
   async getAllBeers(): Promise<Beer[]> {
-    return JSON.parse(JSON.stringify(this.beers));
+    const filePath = process.env.BEERS_JSON_PATH;
+    if (!filePath) {
+      throw new Error("BEERS_JSON_PATH is not defined in the .env file");
+    }
+    const absolutePath = path.resolve(filePath);
+    return JSON.parse(readFileSync(absolutePath, "utf-8"));
   }
 
   async getBeer(beer: string): Promise<Beer | undefined> {
-    return this.beers.find((b) => b.name === beer);
+    const filePath = process.env.BEERS_JSON_PATH;
+    if (!filePath) {
+      throw new Error("BEERS_JSON_PATH is not defined in the .env file");
+    }
+    const absolutePath = path.resolve(filePath);
+    const beers = JSON.parse(readFileSync(absolutePath, "utf-8"));
+    return beers.find((b: { name: string }) => b.name === beer);
   }
 }
