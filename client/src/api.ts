7051ec156aa6440f966e150b96c2b13f5169e435
erbergli <erik.berglind@gmail.com>
2025-02-24 10:29:28 +0100

    add error handling

--- client/src/api.ts
+++ client/src/api.ts
@@ -3,14 +3,30 @@ import { Beer } from './interfaces/beer';
 
 const BASE_URL = "http://localhost:8080";
 
-export async function getAllBeers() : Promise<Beer[]> {
+export async function getAllBeers() : Promise<Beer[] | null> {
+  try{
     const response = await axios.get<Beer[]>(`${BASE_URL}/beer`);
     return response.data;
+  }
+  catch(e : any){
+    console.error(`failed to fetch beers`, e);
+    return null;
+  }
 }
 
-export async function getBeer(name : string) : Promise<Beer>{
-    const response = await axios.get<Beer>(`${BASE_URL}/beer/${name}`);
-    return response.data;
+export async function getBeer(name : string) : Promise<Beer |null>{
+    if(!name.trim()){
+      console.warn("getBeer called with an empty name.");
+      return null;
+    }
+    try{
+      const response = await axios.get<Beer>(`${BASE_URL}/beer/${name}`);
+      return response.data;
+    }
+    catch(e : any){
+      console.error(`failed to fetch beer: ${name}`, e);
+      return null;
+    }
 }
 
 export async function registerNewUser(
