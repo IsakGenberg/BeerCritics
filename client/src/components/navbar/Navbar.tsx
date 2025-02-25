caa23a7623dd6ca36e4486091b33b01fc44cb2a4
hannistuis <hannah.tuu.hannah@gmail.com>
2025-02-25 13:16:47 +0100

    isLoggedIn in navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,7 +1,15 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import "./navbar.css";
 
 function MyNavbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    setIsLoggedIn(!!token);
+  }, []);
+
   return (
     <Navbar expand="lg" className="navbar">
       <Container fluid>
@@ -10,6 +18,10 @@ function MyNavbar() {
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="#">Top Beers</Nav.Link>
           <Nav.Link href="#">All Beers</Nav.Link>
+          {isLoggedIn && <Nav.Link href="#">My Reviews</Nav.Link>}
+          <Nav.Link href={isLoggedIn ? "#" : "/login"}>
+            {isLoggedIn ? "Logout" : "My Account"}
+          </Nav.Link>
           <Nav.Link href="#">My Reviews</Nav.Link>
           <Nav.Link href="/login">My Account</Nav.Link>
         </Nav>
