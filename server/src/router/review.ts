c662dc295f4c015a5654043661d99cc7fa450afc
epicreach <luqas.lundahl4817@gmail.com>
2025-02-25 14:00:18 +0100

    added authentication for myreviews

--- server/src/router/review.ts
+++ server/src/router/review.ts
@@ -14,6 +14,10 @@ reviewRouter.get(
   "/myreviews",
   async (req: ReviewRequest, res: Response<Review[] | string>) => {
     try {
+      if (!req.session.username) {
+        res.status(401).send("Not logged in");
+        return;
+      }
       const reviews = await reviewService.getReviewsUser(req.session.username);
       res.status(200).send(reviews);
     } catch (e: any) {
