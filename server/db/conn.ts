76f46cc2b2ea0a7d2287d3a4ef67d005a3da3ff2
epicreach <luqas.lundahl4817@gmail.com>
2025-03-04 14:09:38 +0100

    fixed env loading

--- server/db/conn.ts
+++ server/db/conn.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-dotenv.config(); // Automatically loads .env from the same directory
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not defined");
