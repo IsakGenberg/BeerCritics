bcde2fb205625f823a04e9e8330c6fb811b8014e
hannistuis <hannah.tuu.hannah@gmail.com>
2025-03-13 16:00:32 +0100

    homepage about

--- client/src/pages/homepage/HomePage.tsx
+++ client/src/pages/homepage/HomePage.tsx
@@ -10,7 +10,7 @@ const beers = await getAllBeers();
 const sortedBeers = beers.sort((a, b) => b.rating - a.rating);
 
 /**
- * 
+ *
  * @returns HomePage component which displays the search bar and the top rated beers in the database
  */
 function HomePage() {
@@ -18,21 +18,39 @@ function HomePage() {
     <Container className="homepage">
       <h1 className="text-beer1">Find the best beers here!🍺</h1>
       <SearchBar />
-      <h2 className="text-beer2">Your top rated beers:</h2>
-      <Row>
-        {sortedBeers.map((beer) => (
-          <Col key={beer.name} xs={12} md={6} lg={3} className="mb-4">
-            <BeerCard
-              imagePath={beer.imagePath}
-              name={beer.name}
-              brewery={beer.brewery}
-              style={beer.style}
-              abv={beer.abv}
-              rating={beer.rating}
-            />
-          </Col>
-        ))}
-      </Row>
+      <h2 className="text-beer2">Featured beers:</h2>
+      <div className="carousel">
+        <Row>
+          {sortedBeers.map((beer) => (
+            <Col key={beer.name} xs={12} md={6} lg={3} className="mb-4">
+              <BeerCard
+                imagePath={beer.imagePath}
+                name={beer.name}
+                brewery={beer.brewery}
+                style={beer.style}
+                abv={beer.abv}
+                rating={beer.rating}
+              />
+            </Col>
+          ))}
+        </Row>
+      </div>
+
+      <div className="about-section">
+        <h1 className="about-title">About</h1>
+        <p className="about-text">
+          BeerCritics is a cutting-edge platform dedicated to beer enthusiasts
+          worldwide. Founded in 2024 by a team of passionate brewers and tech
+          innovators, our mission is to create a vibrant community where beer
+          lovers can discover, review, and discuss their favorite brews. Our
+          platform allows users to explore a vast selection of beers, rate and
+          critique them, and connect with like-minded individuals. With an
+          intuitive interface, expert recommendations, and user-driven rankings,
+          BeerCritics Inc. helps both casual drinkers and connoisseurs find the
+          best beers. At BeerCritics, we believe that every sip tells a
+          story—join us in shaping the ultimate beer experience!
+        </p>
+      </div>
     </Container>
   );
 }
